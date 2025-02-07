83220322617a49a328a452b202a85298befca7da
Tejas Wagh <TejasW@winjittechnologiespvtltd.local>
2025-02-07 15:34:21 +0530

    create a middleware

---
+++ Backend/middleware/fetchUser.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+
+const fetchUser = async (req, res, next) => {
+  //get the user ffrom the jwt token and add id to req object
+  try {
+    const token = req.header('auth-token');
+    if (!token) {
+      res
+        .status(401)
+        .send({ error: 'Please authenticate using a valid token' });
+    }
+
+    const database = jwt.verify(token, process.env.S_KEY);
+
+    req.user = database.user;
+
+    next();
+  } catch (error) {
+    res.status(401).send({ error: '>>>>>>> Please authenticate using a valid token' });
+  }
+};
+
+export default fetchUser;
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -3,8 +3,10 @@ import mongoose from 'mongoose';
 import User from '../models/User.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import fetchUser from '../middleware/fetchUser.js';
 const router = express.Router();
 
+//register route
 router.post('/register', async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -24,30 +26,79 @@ router.post('/register', async (req, res) => {
     // Create a new user
     const newUser = new User({ name, email, password: hashedPassword });
 
-
     // Save user to database
     await newUser.save();
+    const mydata = {
+      user: {
+        id: newUser.id,
+      },
+    };
 
     const sKey = process.env.S_KEY;
-    const authToken = jwt.sign(
-      {
-        data: newUser,
-      },
-      sKey
-    );
+    const authToken = jwt.sign(mydata, sKey);
 
-
-    res
-      .status(201)
-      .json({ message: 'User created successfully', user: authToken });
+    res.status(200).send({ message: 'User created successfully', authToken });
   } catch (error) {
+    console.log(error);
+
     res.status(500).json({ message: 'Error creating user', error });
   }
 });
 
-// Test route
-router.get('/', (req, res) => {
-  res.status(200).send({ message: 'Auth route working!' });
+// login route
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: 'All fields are required!' });
+    }
+
+    // Check if user already exists
+    const newUser = await User.findOne({ email });
+
+    if (!newUser) {
+      return res.status(400).json({ message: 'User not exists' });
+    }
+
+    const passwordCompaire = await bcrypt.compare(password, newUser.password);
+
+    if (!passwordCompaire) {
+      return res.status(400).json({ message: 'Please try again' });
+    }
+
+    const mydata = {
+      user: {
+        id: newUser.id,
+      },
+    };
+    const sKey = process.env.S_KEY;
+    const authToken = jwt.sign(mydata, sKey);
+
+    res
+      .status(200)
+      .send({ message: 'Login the user successfully!', authToken });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({ message: 'Error creating user', error });
+  }
 });
+
+// get userDetailes
+
+router.post('/getUser', fetchUser, async (req, res) => {
+  try {
+    const userId = req.user.id;
+    
+    const user = await User.findById(userId).select('-password');
+    res.status(200).send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error creating user', error });
+  }
+});
+
+
 
 export default router;
