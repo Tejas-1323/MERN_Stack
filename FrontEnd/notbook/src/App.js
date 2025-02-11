edbc86891f57d47346d5d70ced48da8f427d5256
Tejas Wagh <TejasW@winjittechnologiespvtltd.local>
2025-02-11 11:16:44 +0530

    create a componuntes

--- FrontEnd/notbook/src/App.js
+++ FrontEnd/notbook/src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Navebar from "./components/Navebar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-  <h1>This is iNotebook</h1>
-    </div>
+    <>
+      <Router>  
+        <Navebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ FrontEnd/notbook/src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Aboute() {
+  return (
+    <div>this is aboute</div>
+  )
+}
+
+export default Aboute
